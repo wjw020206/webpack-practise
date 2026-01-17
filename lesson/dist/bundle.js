f0ce249359060f854f49517d6b49b8671199321e
CodePencil <jiawei.wang2021@gmail.com>
2026-01-17 14:28:39 +0800

    feat: 学习 webpack 配置文件中 mode 的使用以及 entry 完整写法

--- lesson/dist/bundle.js
+++ lesson/dist/bundle.js
@@ -1,1 +1,92 @@
-(()=>{var e={24(e){e.exports=function(){var e=document.getElementById("root"),t=document.createElement("div");t.innerText="sidebar",e.append(t)}},585(e){e.exports=function(){var e=document.getElementById("root"),t=document.createElement("div");t.innerText="content",e.append(t)}},611(e){e.exports=function(){var e=document.getElementById("root"),t=document.createElement("div");t.innerText="header",e.append(t)}}},t={};function n(r){var o=t[r];if(void 0!==o)return o.exports;var d=t[r]={exports:{}};return e[r](d,d.exports,n),d.exports}var r=n(611),o=n(24),d=n(585);new r,new o,new d})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/content.js"
+/*!************************!*\
+  !*** ./src/content.js ***!
+  \************************/
+(module) {
+
+eval("{function Content() {\n  var dom = document.getElementById('root')\n  var content = document.createElement('div')\n  content.innerText = 'content'\n  dom.append(content)\n}\n\nmodule.exports = Content\n\n\n//# sourceURL=webpack://lesson/./src/content.js?\n}");
+
+/***/ },
+
+/***/ "./src/header.js"
+/*!***********************!*\
+  !*** ./src/header.js ***!
+  \***********************/
+(module) {
+
+eval("{function Header() {\n  var dom = document.getElementById('root')\n  var header = document.createElement('div')\n  header.innerText = 'header'\n  dom.append(header)\n}\n\nmodule.exports = Header\n\n\n//# sourceURL=webpack://lesson/./src/header.js?\n}");
+
+/***/ },
+
+/***/ "./src/index.js"
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval("{// ES Module 默认引入方式\n// import Header from './header.js'\n// import Sidebar from './sidebar.js'\n// import Content from './content.js'\n\n// CommonJS 模块引入规范\nvar Header = __webpack_require__(/*! ./header.js */ \"./src/header.js\")\nvar Sidebar = __webpack_require__(/*! ./sidebar.js */ \"./src/sidebar.js\")\nvar Content = __webpack_require__(/*! ./content.js */ \"./src/content.js\")\n\nnew Header()\nnew Sidebar()\nnew Content()\n\n\n//# sourceURL=webpack://lesson/./src/index.js?\n}");
+
+/***/ },
+
+/***/ "./src/sidebar.js"
+/*!************************!*\
+  !*** ./src/sidebar.js ***!
+  \************************/
+(module) {
+
+eval("{function Sidebar() {\n  var dom = document.getElementById('root')\n  var sidebar = document.createElement('div')\n  sidebar.innerText = 'sidebar'\n  dom.append(sidebar)\n}\n\nmodule.exports = Sidebar\n\n\n//# sourceURL=webpack://lesson/./src/sidebar.js?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Check if module exists (development only)
+/******/ 		if (__webpack_modules__[moduleId] === undefined) {
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path') // node.js 的 path 核心模块
 
 module.exports = {
-  entry: './src/index.js', // 打包的入口文件,默认问 index.js
+  mode: 'production', // 模式, 'production' 打包会压缩代码，'development' 不会，默认是 'production', 不添加 mode，打包时控制台会有警告
+  // entry: './src/index.js', // 打包的入口文件,默认为 index.js
+  entry: {
+    main: './src/index.js', // 该写法是上一行代码的完整写法，main 为 chunk 名
+  },
   output: {
     filename: 'bundle.js', // 打包输出的文件名，默认为 main.js
     path: path.resolve(__dirname, 'dist'), // 打包输出的目录名(绝对路径)，默认为 dist
