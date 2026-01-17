fad719e4ec734ba68d9a0411bbc8283a1706bfca
CodePencil <jiawei.wang2021@gmail.com>
2026-01-17 14:15:36 +0800

    feat: 增加了配置文件中的注释

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path') // node.js 的 path 核心模块
 
 module.exports = {
-  entry: './src/index.js', // 打包的入口文件
+  entry: './src/index.js', // 打包的入口文件,默认问 index.js
   output: {
-    filename: 'bundle.js', // 打包输出的文件名
+    filename: 'bundle.js', // 打包输出的文件名，默认为 main.js
     path: path.resolve(__dirname, 'dist'), // 打包输出的目录名(绝对路径)，默认为 dist
   },
 }
