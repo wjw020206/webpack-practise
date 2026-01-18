1499b5e1895959e8d9dee174bf2f715e04cc40be
CodePencil <jiawei.wang2021@gmail.com>
2026-01-18 09:03:46 +0800

    feat: 学习了 file-loader 和 url-loader 的基本配置

--- lesson/dist/bundle.js
+++ lesson/dist/bundle.js
@@ -1,1 +1,85 @@
-(()=>{"use strict";var t={};t.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),(()=>{var r;t.g.importScripts&&(r=t.g.location+"");var e=t.g.document;if(!r&&e&&(e.currentScript&&"SCRIPT"===e.currentScript.tagName.toUpperCase()&&(r=e.currentScript.src),!r)){var c=e.getElementsByTagName("script");if(c.length)for(var n=c.length-1;n>-1&&(!r||!/^http(s?):/.test(r));)r=c[n--].src}if(!r)throw new Error("Automatic publicPath is not supported in this browser");r=r.replace(/^blob:/,"").replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),t.p=r})();const r=t.p+"e49823921af3dad34a48.jpg";var e=new Image;e.src=r,document.getElementById("root").append(e)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/avatar.jpg"
+/*!************************!*\
+  !*** ./src/avatar.jpg ***!
+  \************************/
+(module) {
+
+eval("{module.exports = \"images/avatar_e49823921af3dad34a48.jpg\";\n\n//# sourceURL=webpack://lesson/./src/avatar.jpg?\n}");
+
+/***/ },
+
+/***/ "./src/index.js"
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./avatar.jpg */ \"./src/avatar.jpg\");\n/* ES Module 默认引入方式\nimport Header from './header.js'\nimport Sidebar from './sidebar.js'\nimport Content from './content.js'\n\nCommonJS 模块引入规范\nvar Header = require('./header.js')\nvar Sidebar = require('./sidebar.js')\nvar Content = require('./content.js')\nvar avatar = require('./avatar.jpg') // 资源模块对图片进行了处理\n\nconsole.log(avatar)\n\nnew Header()\nnew Sidebar()\nnew Content() */\n\n // 资源模块对图片进行了处理\n\nvar img = new Image()\nimg.src = _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__\n\nvar root = document.getElementById('root')\nroot.append(img)\n\n\n//# sourceURL=webpack://lesson/./src/index.js?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Check if module exists (development only)
+/******/ 		if (__webpack_modules__[moduleId] === undefined) {
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path') // node.js 的 path 核心模块
 
 module.exports = {
-  mode: 'production', // 模式, 'production' 打包会压缩代码，'development' 不会，默认是 'production', 不添加 mode，打包时控制台会有警告
+  mode: 'development', // 模式, 'production' 打包会压缩代码，'development' 不会，默认是 'production', 不添加 mode，打包时控制台会有警告
   // entry: './src/index.js', // 打包的入口文件,默认为 index.js
   entry: {
     main: './src/index.js', // 该写法是上一行代码的完整写法，main 为 chunk 名
@@ -9,12 +9,44 @@ module.exports = {
   module: {
     // 配置模块打包规则
     rules: [
-      {
-        test: /\.jpg$/, // .jpg 结尾的文件
-        type: 'asset/resource', // 使用 webpack 5 内置的资源模块，等价于 file-loader
+      /* {
+        test: /\.(jpg|png|gif)$/, // 匹配 .jpg、.png、.gif 结尾的文件
+        type: 'asset/resource', // 使用 webpack 5 内置的资源模块，将文件发送到输出目录，等价于 file-loader
+        generator: {
+          filename: '[name]_[hash][ext]', // （占位符）配置与原始图片的名称和后缀相同，此处 [ext] 前不用加 .，但在 webpack 4 中是需要加 .
+          publicPath: 'images/', // 请求资源访问的路径，也可以是线上的 'https://cdn/assets/'
+          outputPath: 'images/', // 实际资源的路径
+        },
         // webpack 5 之前的版本需要单独安装 loader
         // use: {
         //   loader: 'file-loader',
+        //   options: {
+        //     name: '[name]_[hash].[ext]', // （占位符）配置与原始图片的名称和后缀相同
+        //     outputPath: 'images/',
+        //   },
+        // },
+      }, */
+      {
+        test: /\.(jpg|png|gif)/,
+        type: 'asset', // 使用 webpack 5 内置的资源模块，导出一个资源的 data URI，等价于 url-loader
+        generator: {
+          filename: '[name]_[hash][ext]',
+          publicPath: 'images/',
+          outputPath: 'images/',
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10240, // 等价于 url-loader 的 limit
+          },
+        },
+        // webpack 5 之前的版本需要单独安装 loader
+        // use: {
+        //   loader: 'url-loader',
+        //   options: {
+        //     name: '[name]_[hash].[ext]', // （占位符）配置与原始图片的名称和后缀相同
+        //     outputPath: 'images/',
+        //     limit: 2048, // 当图片大小超过 2kb(2048Byte) 时，使用 file-loader 一样的效果，反之则使用 base64
+        //   },
         // },
       },
     ],
